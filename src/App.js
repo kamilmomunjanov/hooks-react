7382e4ce749395e46c01242c61ed5d1dea7c77cd
Kamil <kamilmomunjanov@gmail.com>
2024-01-20 02:55:52 +0600

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, useMemo} from "react";
+
+function complexCompute(num) {
+    let i = 0
+    while (i < 1000000000) i++
+    return num * 2
+}
 
 
 function App() {
-    const [count, setCount] = useState(1)
+    const [number, setNumber] = useState(42)
+    const [colored, setColored] = useState(false)
 
+    const styles = useMemo(() => {
+        return {
+            color: colored ? "dark" : "black"
+        }
+    },[colored])
 
+    const computed = useMemo(() => {
+        complexCompute(number)
+    },[number])
 
+    useEffect(() => {
+        console.log("Styles changed")
+    },[styles])
 
 
     return (
-        <div>
-
-        </div>
+        <>
+            <h1>Вычисляемое свойство: {computed}</h1>
+            <button onClick={() => setNumber(prevState => prevState + 1)}>Add</button>
+            <button onClick={() => setNumber(prevState => prevState - 1)}>Remove</button>
+            <button onClick={() => setColored(prevState => !prevState)}>Change</button>
+        </>
     );
 }
 
